4f4efd4459a9ed1036004be59dd58873d4bfa8aa
Davit Tavadze <davittavadze31@gmail.com>
2019-01-21 13:07:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  Tashal
@@ -7,67 +8,94 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    int quantity = 0;
-    int unit = 0;
-    double length = 0;
-    
-    
-    
-    cout<<"Choose a quantity: "
-    <<endl<<"1.Length"<<endl<<"2.Mass"<<endl<<"3.Tempreture"
-    <<endl<<"Quantity: ";cin>>quantity;
-    cin>>quantity;
-    
-    if(quantity == 1)
-    {
-        cout<<"Choose an unit: "<<endl<<"1.Nanometer"
-        <<endl<<"2.Micrometre"
-        <<endl<<"3.Milimetre"
-        <<endl<<"4.Centimetre"
-        <<endl<<"5.Decimetre"
-        <<endl<<"6.Metre"
-        <<endl<<"7.Decametre"
-        <<endl<<"8.Hectometre"
-        <<endl<<"9.Kilometre"
-        <<endl<<"10.Mil"
-        <<endl<<"11.Inch"
-        <<endl<<"12.Foot"
-        <<endl<<"13.Yard"
-        <<endl<<"14.Mile"
-        <<endl<<"Unit: ";
-        cin>>unit;
-        
-        if(unit == 1){
-            cout<<"Ammount: "<<endl;
-            cin>>length;
-            
-            cout<<length<<" Nanometre = "<<length / 1000<<"Micrometre"
-            <<endl<<length<<" Nanometre = "<<length / 1000000<<"Milimetre"
-            <<endl<<length<<" Nanometre = "<<length / 10000000<<"Centimetre"
-            <<endl<<length<<" Nanometre = "<<length / 100000000<<"Decimetre"
-            <<endl<<length<<" Nanometre = "<<length / 1000000000<<"Metre"
-            <<endl<<length<<" Nanometre = "<<length / 10000000000<<"Decametre"
-            <<endl<<length<<" Nanometre = "<<length / 100000000000<<"Hectometre"
-            <<endl<<length<<" Nanometre = "<<length / 1000000000000<<"Kilometre"
-            <<endl<<length<<" Nanometre = "<<length / 10000000000000<<"Mil"
-            <<endl<<length<<" Nanometre = "<<length / 2.54*100000000<<"inch"
-            <<endl<<length<<" Nanometre = "<<length / 3.048*1000000000<<"Foot"
-            <<endl<<length<<" Nanometre = "<<length / 9.144*1000000000<<"Yard"
-            <<endl<<length<<" Nanometre = "<<length / 1.609*1000000000000<<"Mile";
-            
-            
-            
-        }
-        
-        
-        
-        
-        
-        
-        
-    }
+
+void quantity();void UnitOne();void UnitTwo();void UnitThree();void Celsius();void Farenheit();void Kelvin();
+
+int main()
+{
+    quantity();
     return 0;
 }
+//quantity
+void quantity()
+{
+    int quantity = 0;
+    cout<<"Choose a quantity: "
+    <<endl<<endl<<"1.Length"<<endl<<"2.Mass"<<endl<<"3.Tempreture"
+    <<endl<<endl<<"------------------------------------------------------------------"<<endl<<"Quantity: ";
+    cin>>quantity;
+    if(quantity == 1)
+    {
+        return UnitOne();
+    }
+    if(quantity == 2)
+    {
+        return UnitTwo();
+    }
+    if(quantity == 3)
+    {
+        return UnitThree();
+    }
+}
+//choosing an unit
+void UnitOne()
+{
+    int Lengthunit = 0;
+    cout<<endl<<"Choose an unit: "<<endl<<"1.Nanometer\n2.Micrometre\n3.Milimetre"
+    <<endl<<"4.Centimetre\n5.Decimetre\n6.Metre\n7.Decametre\n8.Hectometre\n9.Kilometre\n10.Mil\n11.Inch\n12.Foot\n13.Yard\n14.Mile"<<endl<<endl<<"Unit: ";
+    cin>>Lengthunit;
+}
+void UnitTwo()
+{
+    int MassUnit = 0;
+    cout<<endl<<"Choose an unit: "
+    <<endl<<"1.Nanogram\n2.Microgram\n3.Milligram\n4.Centigram\n5.Decigram\n6.Gram\n7.Decagram\n8.Hectogram\n9.Kilogram\n10.Ton\n11.Ounce\n12.Pound"<<endl<<endl<<"Unit: ";
+    cin>>MassUnit;
+    
+}
+void UnitThree()
+{
+    int TempUnit = 0;
+    cout<<endl<<"Choose an unit\n1.Celsius\n2.Farenheit\n3.Kelvin\n\n"<<"Unit: ";
+    cin>>TempUnit;
+    if(TempUnit == 1)
+    {
+        return Celsius();
+    }
+    if(TempUnit == 2)
+    {
+        return Farenheit();
+    }
+    if(TempUnit == 3)
+    {
+        return Kelvin();
+    }
+}
+//tempclass
+void Celsius()
+{
+    double c = 0;
+    cout<<"Ammount: ";
+    cin>>c;
+    cout<<endl<<c<<"Celsius = "<<((c* 1.8)+32)  << " Farenheit."<<endl<<c<<" Celsius = "<< (c + 273.15)  << " Kelvin."
+    << endl << endl;
+}
+void Farenheit()
+{
+    double f = 0;
+    cout<<"Ammount: ";
+    cin>>f;
+    cout<<endl<<f<<"Farenheit = "<<((f - 32) / 1.8 )<<"Celsius."<<endl<<f<<"Farenheit = "<<(((f - 32)/1.8)+273.15)
+    <<"Kelvin";
+}
+void Kelvin()
+{
+    double k = 0;
+    cout<<"Ammount: ";
+    cin>>k;
+    cout<<endl<<k<<"Kelvin = "<<( k - 273.15 )<<" Celsius"<<endl<<k<<"Kelvin = "<<((k - 273.15) * 1.8 + 32)<<"Farenheit";
+}
+
